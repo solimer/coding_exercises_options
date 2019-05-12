090b86f6bab2ae0fdcdbf729075d3f23b8fd4483
Rafael Paz <rafael@adikteev.com>
2019-05-12 15:29:17 +0200

    setup test env

--- filtering_matches/server/index.js
+++ filtering_matches/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const routes = require('./routes');
+const routes = require('./lib/routes');
 
 const app = express();
 const port = process.env.PORT || 5000;
---
+++ filtering_matches/server/test/unit/filters/booleanFilter.js
@@ -0,0 +1,8 @@
+const BooleanFilter = require('../../../lib/filters/booleanFilter');
+
+describe('Boolean Filter', () => {
+  it('should return bla', () => {
+    const one = 1;
+    one.should.equal(1);
+  });
+});
